d65d82daa60377bab8a980b4f50c437b9cdbf3f3
Khawk2 <ksalapeg.270700@gmail.com>
2025-01-21 00:51:49 -0500

    Creación función agregar amigo

--- app.js
+++ app.js
@@ -2,3 +2,31 @@
 
 let friends = [];
 
+// Función para agregar un amigo
+function addFriend() {
+    //Obtener el nombre del amigo
+    let friend = document.getElementById('amigo');
+    let friendName = friend.value.trim();
+
+    //Validar que el nombre no esté vacío
+    if (friendName === ""){
+        alert("Por favor, inserte un nombre");
+        return;
+    }
+
+    //Validar que el nombre contenga solo letras y espacios y no números
+    const regex = /^[a-zA-Z\s]+$/;
+    if (!regex.test(friendName)){
+        alert("Por favor, inserte un nombre válido");
+        return;
+    }
+
+    //Agregar el nombre a la lista
+    friends.push(friendName);
+
+    //Actualizar la lista de amigos y que pueda ser visible
+    updateFriendsList();
+
+    //Limpiar el campo de texto(input)
+    friend.value = "";
+}
